21b9807de7f7da0950947fa46789aa55ddbb2dd5
katewindy <roboticbrain@gmail.com>
2014-09-18 23:50:48 -0600

    Revert "Added buttons, wrote jQuery to popdown edit form"
    This reverts commit ad18a89886de9abf436fa86628127119ecca2e97.

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
 $(document).on('ready', function() {
-
-	$('.form').hide();
-
-  	$('#profilebtn').on('click', function(){
-  		$(this).hide();
-  		$('.form').slideDown('slow');
-  		$('#editname').val($('#name').text());
-		$('#editbio').val($('#bio').text());
-		$('#editbooks').val($('#books').text());
-		$('#editjslib').val($('#jslib').text());
-
-  	});
-
-  	$('#editbtn').on('click', function(){
-  		$('.form').slideUp('slow');
-  		$('#profilebtn').show();
-  		$('#name').text($('#editname').val());
-		$('#bio').text($('#editbio').val());
-		$('#books').text($('#editbooks').val());
-		$('#jslib').text($('#editjslib').val());
-  	});
-
-  	
+  
 });
